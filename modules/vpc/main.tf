40ca792f2f7193714f6f2afd65a03e5958c37443
Tynar <tynar.esenbaeva@slalom.com>
2023-03-10 13:07:01 -0500

    updated sec group cidr block

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -64,7 +64,7 @@ resource "aws_security_group" "vpc_task" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["73.143.65.89/32"]
   }
 
 
