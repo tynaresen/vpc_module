9dce8bd107ab5b01f1404510ac49351460c1b4ee
Tynar <tynar.esenbaeva@slalom.com>
2023-03-10 13:09:46 -0500

    commented out the sec group

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -54,25 +54,25 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-#Security Group 
-resource "aws_security_group" "vpc_task" {
-  name        = "vpc_task"
-  description = "Allow TLS inbound traffic"
+# #Security Group 
+# resource "aws_security_group" "vpc_task" {
+#   name        = "vpc_task"
+#   description = "Allow TLS inbound traffic"
 
-  ingress {
-    description = "Allow ssh"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["73.143.65.89/32"]
-  }
+#   ingress {
+#     description = "Allow ssh"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["73.143.65.89/32"]
+#   }
 
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-}
+# }
